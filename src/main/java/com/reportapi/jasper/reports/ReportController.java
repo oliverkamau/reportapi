e11674c6a83adcc1093da0885da6ac445409ef05
gabudev <70848401+oliverkamau@users.noreply.github.com>
2022-02-05 11:24:41 +0300

    adjustments to report

--- src/main/java/com/reportapi/jasper/reports/ReportController.java
+++ src/main/java/com/reportapi/jasper/reports/ReportController.java
@@ -2,6 +2,12 @@ package com.reportapi.jasper.reports;
 
 import com.alibaba.fastjson.JSONObject;
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.Exporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
+import org.apache.commons.io.FileUtils;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,10 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletContext;
 import javax.sql.DataSource;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigDecimal;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -36,11 +42,39 @@ public class ReportController {
                                    @RequestParam(required = false) String report,
                                    @RequestParam(required = false) BigDecimal balance,
                                    @RequestParam(required = false) String path,
-                                   @RequestParam(required = false) String paymentdate
+                                   @RequestParam(required = false) String paymentdate,
+                                   @RequestParam(required = false) String format
                                   ) throws JRException, SQLException, IOException {
+        JSONObject j = new JSONObject();
+        if(format.equalsIgnoreCase("pdf")) {
+            String p = "C:\\pdf\\" + report + ".pdf";
+//        String p= "C:\\pdf\\"+report+".pdf";
+            System.out.println(path);
+            File f = new File(path);
+            byte[] logo = Files.readAllBytes(Paths.get(path));
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("logo", logo);
+            parameters.put("balance", balance);
+            parameters.put("payment", payment);
+            parameters.put("receiptdate", paymentdate);
 
-        String p= "C:\\pdf\\"+report+".pdf";
-        System.out.println(path);
+
+            boolean exist = Files.deleteIfExists(Paths.get("C:\\pdf\\" + report + ".pdf"));
+            if (exist) {
+                System.out.println("File exists and was deleted");
+            } else {
+                System.out.println("File does not exist and was not deleted");
+            }
+
+            File file = ResourceUtils.getFile("classpath:reports/" + report + ".jrxml");
+            JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource.getConnection());
+            JasperExportManager.exportReportToPdfFile(jasperPrint, p);
+            j.put("path",p);
+        }
+        else if(format.equalsIgnoreCase("excel")) {
         File f = new File(path);
         byte[] logo = Files.readAllBytes(Paths.get(path));
         Map<String, Object> parameters = new HashMap<>();
@@ -48,24 +82,34 @@ public class ReportController {
         parameters.put("balance", balance);
         parameters.put("payment", payment);
         parameters.put("receiptdate", paymentdate);
+        File file = ResourceUtils.getFile("classpath:reports/" + report + ".jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource.getConnection());
 
+            SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
+        configuration.setOnePagePerSheet(true);
+        configuration.setIgnoreGraphics(false);
 
-        boolean exist = Files.deleteIfExists(Paths.get("C:\\pdf\\"+report+".pdf"));
-        if(exist){
-            System.out.println("File exists and was deleted");
+        File outputFile = new File("gender_distribution.xlsx");
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             OutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+            Exporter exporter = new JRXlsxExporter();
+            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(byteArrayOutputStream));
+            exporter.setConfiguration(configuration);
+            exporter.exportReport();
+            byteArrayOutputStream.writeTo(fileOutputStream);
         }
-        else{
-            System.out.println("File does not exist and was not deleted");
+
+        Path src = Paths.get(report+".xlsx");
+        boolean exist = Files.deleteIfExists(Paths.get("C:\\excel\\" + report + ".xlsx"));
+        Path dest = Paths.get("C:\\excel\\" + report + ".xlsx");
+        FileUtils.copyFile(src.toFile(), dest.toFile());
+        boolean rm = Files.deleteIfExists(Paths.get(report+".xlsx"));
+        j.put("path","C:\\excel\\" + report + ".xlsx");
+
         }
 
-        File file = ResourceUtils.getFile("classpath:reports/"+report+".jrxml");
-                JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,parameters,dataSource.getConnection());
-        JasperExportManager.exportReportToPdfFile(jasperPrint,p);
-
-        JSONObject j = new JSONObject();
-        j.put("path",p);
 
         return j;
 
